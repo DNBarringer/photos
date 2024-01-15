81a3321d924e17806985a43bfa1ab877b32dd5b3
DNBarringer <davidnbarringer@gmail.com>
2024-01-14 20:03:07 -0700

    responsive font

--- vite-project/src/components/PageTitle.vue
+++ vite-project/src/components/PageTitle.vue
@@ -1,3 +1,15 @@
 <template>
-    <p class="mt-4 ml-16" style="font-family: 'Cardo', serif; font-size: 4rem;">David Barringer</p>
-</template>
+    <p class="mt-4 ml-16 custom-font" :style="{ fontFamily: 'Cardo, serif', fontSize: fontSize }">
+      David Barringer
+    </p>
+  </template>
+  
+<script setup lang="ts">
+import { ref } from 'vue';
+
+const fontSize = ref(window.innerWidth <= 600 ? '1.5rem' : '4rem');
+
+window.onresize = () => {
+fontSize.value = window.innerWidth <= 600 ? '1.5rem' : '4rem';
+};
+  </script>
