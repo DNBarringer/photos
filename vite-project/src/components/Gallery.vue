16a9b46ac9ab8fd69706737db77a437b7a92c7db
dbarringer <dbarringer@helcim.com>
2024-02-24 13:48:18 -0700

    cleanup

--- vite-project/src/components/Gallery.vue
+++ vite-project/src/components/Gallery.vue
@@ -5,39 +5,39 @@
       :text="mountainText"
       :bg_image="bg_image"
       :additionalStyle="{ bottom: '5%' }"
-    ></PhotoCardLargeLandscape>
+    />
     <PhotoCardLargePortrait
       :image="spaceImage"
       :text="spaceText"
       :bg_image="bg_image"
       :additionalStyle="{ right: '5%', bottom: '5%' }"
-    ></PhotoCardLargePortrait>
+    />
     <VCard
       height="100vh"
       width="100vw"
-      > 
+    > 
       <VImg :src="morganImage" cover position="center 80%"></VImg>
       <div class="my-back" :style="{ backgroundImage: `url(${bg_image})`, marginLeft: '20px', position: 'absolute', top: '5%' }">
         <p>{{ morganText }}</p>
       </div>
-      </VCard>
+    </VCard>
   </div>
 </template>
 
 
 <script setup lang="ts">
-import PhotoCardLargeLandscape from './photocard/Landscape.vue';
-import PhotoCardLargePortrait from './photocard/Portrait.vue';
+  import PhotoCardLargeLandscape from './photocard/Landscape.vue';
+  import PhotoCardLargePortrait from './photocard/Portrait.vue';
 
-const spaceText = 'Praire\nMountain';
-const spaceImage = new URL('../assets/images/space.jpg', import.meta.url).href;
+  const spaceText = 'Praire\nMountain';
+  const spaceImage = new URL('../assets/images/space.jpg', import.meta.url).href;
 
-const mountainText = 'Icefield Parkway';
-const mountainImage = new URL('../assets/images/icefield_mountains.jpg', import.meta.url).href;
-const bg_image = new URL('../assets/images/icefield_mountains2.jpeg', import.meta.url).href;
+  const mountainText = 'Icefield Parkway';
+  const mountainImage = new URL('../assets/images/icefield_mountains.jpg', import.meta.url).href;
+  const bg_image = new URL('../assets/images/icefield_mountains2.jpeg', import.meta.url).href;
 
-const morganText = 'Morgan';
-const morganImage = new URL('../assets/images/morgan.jpg', import.meta.url).href;
+  const morganText = 'Morgan';
+  const morganImage = new URL('../assets/images/morgan.jpg', import.meta.url).href;
 
 </script>
 
